3609a47af052a74f44e2f677287c183a4e655fcf
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-04-18 17:08:29 +0530

    update feature

--- src/component/CartHeader.js
+++ src/component/CartHeader.js
@@ -4,7 +4,7 @@ export const CartHeader = ({totalDeliveryTime, couponApplied, discountPrice, tot
             <h1>Cart</h1>
             <h3>Total delivery time: {totalDeliveryTime} minutes</h3>
             <h3>Total Price: ${couponApplied ? discountPrice : totalPrice} </h3>
-            <button className="home-btn" onClick={() => setCouponApplied(!couponApplied)}>{couponApplied ? 'Remove Coupon' : 'Apply Coupon'}</button>
+            <button disabled={totalPrice===0} className="home-btn" onClick={() => setCouponApplied(!couponApplied)}>{couponApplied ? 'Remove Coupon' : 'Apply Coupon'}</button>
         </>
     )
 }
